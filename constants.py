15592f772ebb6cb81d8e4627a8d5d610cded3015
Devon Burnham <novedevo@gmail.com>
2020-03-10 22:13:46 -0700

    added laws

--- constants.py
+++ constants.py
@@ -7,10 +7,22 @@ na=6.0221415e23
 cal2j = 4.184
 j2cal = 1/4.184
 c=299792458 # m/s
+Rjoules = 8.31446261815324
+Ratm = 0.082057366080960
+Rtorr = 62.363598221529
+
+
 biotsavart = 'B=(mu0*I)/(2*pi*R)'
-bsk = mu0/2/pi
+faraday = 'emf induced in loop = - dphi/dt'
+ampere = 'integral of B dot dl = mu0 * I enclosed'
+magnetic_field_from_wire = 'B = bsk*I/R'
+
+bsk = 2e-7
 
 class Neutron:
     mass=1.67493e-27 # kg
 class Electron:
     mass=9.10939e-31 # kg
+
+def ln(x):
+    return log(x)
